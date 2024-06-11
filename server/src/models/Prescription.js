34d7926e4c190359f779322a04ac8fe74c5a20a7
Biruk Habte <biruk.habte-ug@aau.edu.et>
2024-06-11 11:51:00 +0300

    Update supplier order tracking

--- server/src/models/Prescription.js
+++ server/src/models/Prescription.js
@@ -77,3 +77,4 @@ prescriptionSchema.index({ issueDate: -1 });
 
 module.exports = mongoose.model('Prescription', prescriptionSchema);
 // Commit on 2024-06-1 at 9:24
+// Commit on 2024-06-11 at 11:51

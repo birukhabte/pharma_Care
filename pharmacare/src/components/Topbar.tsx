14cd0aea4e9425ca65db6b26170705b7eb8b69db
Biruk Habte <biruk.habte-ug@aau.edu.et>
2024-06-12 09:54:00 +0300

    Add medicine dosage information

--- pharmacare/src/components/Topbar.tsx
+++ pharmacare/src/components/Topbar.tsx
@@ -294,3 +294,4 @@ export default function Topbar({ onMobileMenuToggle, mobileMenuOpen }: TopbarPro
 }// Commit on 2024-06-6 at 16:42
 // Commit on 2024-06-8 at 9:41
 // Commit on 2024-06-12 at 18:56
+// Commit on 2024-06-12 at 9:54

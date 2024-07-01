bee73396b38cc8444618a465d4b6ba66408b67dc
Biruk Habte <biruk.habte-ug@aau.edu.et>
2024-07-01 15:29:00 +0300

    Update customer insurance verification

--- server/src/routes/customers.js
+++ server/src/routes/customers.js
@@ -120,3 +120,4 @@ router.patch('/:id/loyalty', authenticate, async (req, res) => {
 module.exports = router;
 // Commit on 2024-06-3 at 12:34
 // Commit on 2024-06-21 at 16:49
+// Commit on 2024-06-31 at 15:29
